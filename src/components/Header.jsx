4385eaa223529470bc58b55e647340bc42e0a028
AKDev32 <amankumar005562@gmail.com>
2025-09-14 15:13:32 +0530

    Added Header and Main

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chefclaudeLogo from '/images/chef-claude-icon.png';
+
+export default function Header () {
+    return (
+        <header>
+            <img src={chefclaudeLogo} />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Main () {
+
+    return (
+        <form className="add-ingredient-form">
+            <input
+                type="textt"
+                placeholder="e. g. oregano"
+                aria-label="Add ingredient"
+            />
+            <button >Add ingredient</button>
+        </form>
+    )
+}
